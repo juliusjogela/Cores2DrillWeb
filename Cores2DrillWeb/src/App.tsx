7dca6d1ae368797300725a7bd630b2f643326f24
Julius Jogela <jogelaj@tcd.ie>
2025-07-22 23:10:05 +0100

    Header and welcome section complete

--- Cores2DrillWeb/src/App.tsx
+++ Cores2DrillWeb/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from "./assets/logo.png"
 import './App.css'
+import Header from "./components/header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+   <div className="app-container">
+    <Header />
+    {/*Welcome section*/}
+    <div className = "welcome-section">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={logo} alt="Cores2Drill Logo" className="cores2drill-logo"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test hey
-        </p>
+      <div className = "welcome-text">
+        <h1>Hi, Welcome to Cores2Drill</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>
+
+
+
+
+
   )
 }
 
---
+++ Cores2DrillWeb/src/components/header.tsx
@@ -0,0 +1,8 @@
+import "../App.css"
+
+function Header() {
+    return (
+        <div className = "header">+353 89 232 3315 | cores2drill@gmail.com | Galway </div>
+    )
+}
+export default Header
