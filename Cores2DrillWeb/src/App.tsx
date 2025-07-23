7fdc5df1ee6f586f242f0e8d73205a8b460d23fd
Julius Jogela <jogelaj@tcd.ie>
2025-07-23 03:13:25 +0100

    New pics and Styling added

--- Cores2DrillWeb/src/App.tsx
+++ Cores2DrillWeb/src/App.tsx
@@ -1,6 +1,7 @@
 import logo from "./assets/logo.png"
 import './App.css'
 import Header from "./components/header"
+import vans from "./assets/cores2drill.png"
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
         <img src={logo} alt="Cores2Drill Logo" className="cores2drill-logo"/>
       </div>
       <div className = "welcome-text">
-        <h1>Hi, Welcome to Cores2Drill</h1>
+      <h1>Cores2Drill!</h1>
+      <p>We are a leading diamond core drilling company operating all over Ireland</p>
       </div>
+        <img src={vans} alt="Cores2Drill Vans" className="cores2drill-vans"/>
     </div>
   </div>
 
--- Cores2DrillWeb/src/components/header.tsx
+++ Cores2DrillWeb/src/components/header.tsx
@@ -2,7 +2,20 @@ import "../App.css"
 
 function Header() {
     return (
-        <div className = "header">+353 89 232 3315 | cores2drill@gmail.com | Galway </div>
+        <div className="header">
+            <div className="header-left">Cores2Drill</div>
+            <div className="header-center">
+                +353 89 232 3315&nbsp;|&nbsp;
+                <a
+                    href="mailto:cores2drill@gmail.com"
+                    style={{ color: "white", fontWeight: "normal", textDecoration: "underline" }}
+                >
+                    cores2drill@gmail.com
+                </a>
+                &nbsp;|&nbsp;Galway
+            </div>
+            <div className="header-right"></div>
+        </div>
     )
 }
 export default Header
